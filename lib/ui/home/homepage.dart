48452b2af455be4798912e7a0f5ea2404d2e6088
neelam <neelam@softserv.in>
2023-01-24 14:57:34 +0530

    changes

--- lib/ui/home/homepage.dart
+++ lib/ui/home/homepage.dart
@@ -3,6 +3,8 @@ import 'dart:ui';
 import 'package:drawingapp/widgets/gettext.dart';
 import 'package:flutter/material.dart';
 import '../paintpages/PaintClass.dart';
+import 'dart:math' as math; // import this
+
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -17,6 +19,8 @@ class _HomePageState extends State<HomePage> {
  final _offsets = <Offset>[];
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.white,
       // appBar: AppBar(
@@ -24,69 +28,125 @@ class _HomePageState extends State<HomePage> {
       //   title: getText("home", 14, FontWeight.w400, Colors.black),
       // ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          GestureDetector(
-              onPanDown: (details){
-                //_offsets.clear();
-                final renderBox =  context.findRenderObject() as RenderBox;
-                final localPostion = renderBox.globalToLocal(details.globalPosition);
-                _offsets.add(localPostion);
-                print('localPostion${localPostion}');
-                setState(() {});
-              },
-              onPanStart: (details){
-                final renderBox =  context.findRenderObject() as RenderBox;
-                final localPostion = renderBox.globalToLocal(details.globalPosition);
-                _offsets.add(localPostion);
-                setState(() {});
-              },
-              onPanUpdate: (details){
-                final renderBox =  context.findRenderObject() as RenderBox;
-                final localPostion = renderBox.globalToLocal(details.globalPosition);
-                _offsets.add(localPostion);
-                print('localPostion on update${localPostion}');
-                setState(() {});
-              },
-              onPanEnd: null,
-              child: ClipRect (
-                clipBehavior: Clip.hardEdge,
-                child: CustomPaint(
-                  painter: FlipBookPainter(_offsets),
-                  //foregroundPainter:FlipBookPainter(_offsets) ,
-                  child:  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height:MediaQuery.of(context).size.height/2-30,
-                    // color: Colors.red[50],
+          SizedBox(
+            width: width,height: height/2-34,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                GestureDetector(
+                    onPanDown: (details){
+                      //_offsets.clear();
+                      final renderBox =  context.findRenderObject() as RenderBox;
+                      final localPostion = renderBox.globalToLocal(details.globalPosition);
+                      _offsets.add(localPostion);
+                      print('localPostion${localPostion}');
+                      setState(() {});
+                    },
+                    onPanStart: (details){
+                      final renderBox =  context.findRenderObject() as RenderBox;
+                      final localPostion = renderBox.globalToLocal(details.globalPosition);
+                      _offsets.add(localPostion);
+                      setState(() {});
+                    },
+                    onPanUpdate: (details){
+                      final renderBox =  context.findRenderObject() as RenderBox;
+                      final localPostion = renderBox.globalToLocal(details.globalPosition);
+                      _offsets.add(localPostion);
+                      print('localPostion on update${localPostion}');
+                      setState(() {});
+                    },
+                    onPanEnd: null,
+                    child: ClipRect (
+                      clipBehavior: Clip.hardEdge,
+                      child: CustomPaint(
+                        painter: FlipBookPainter(_offsets),
+                        //foregroundPainter:FlipBookPainter(_offsets) ,
+                        child:  Container(
+                          width: width/2-3,
+                          height:height/2-1,
+                          // color: Colors.red[50],
 
+                        ),
+                      ),
+                    )
+
+                ),
+                const VerticalDivider(
+                  thickness: 2, color: Colors.black,width: 3,
+                ),
+                ClipRect(
+                  clipBehavior: Clip.hardEdge,
+                  child: Transform(
+                    alignment: Alignment.center,
+                    transform: Matrix4.rotationY(math.pi),
+                    child: CustomPaint(
+                      painter: FlipBookPainter(_offsets),
+                      child: Container(
+                        //color: Colors.blue,
+                        width: width/2,
+                        height:height/2,
+                       // color: Colors.red[50],
+
+                      ),
+                    ),
                   ),
                 ),
-              )
-
+              ]),
           ),
-          const Divider(
-            thickness: 2, color: Colors.black,
-          ),
-          const SizedBox(height: 10,),
-          ClipRect(
-            clipBehavior: Clip.hardEdge,
-            child: CustomPaint(
-                painter: FlipBookPainter(_offsets),
-                child: RotatedBox(
-                  quarterTurns: 4,
-                  child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height:MediaQuery.of(context).size.height/2,
-                  // color: Colors.red[50],
+          const Divider(thickness: 2, color: Colors.black,height: 1,),
+          SizedBox(
+            width: width,height: height/2-33,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                ClipRect(
+                  clipBehavior: Clip.hardEdge,
+                  child: Transform(
+                    alignment: Alignment.center,
+                    //transform: Matrix4.rotationY(math.pi),
+                    transform: Matrix4.rotationX(math.pi),
+                    child: CustomPaint(
+                      painter: FlipBookPainter(_offsets),
+                      child: Container(
+                        width: width/2-3,
+                        height:height/2,
+                       // color: Colors.red[50],
 
+                      ),
+                    ),
+                  ),
                 ),
-              ),
+                const VerticalDivider(
+                  thickness: 2, color: Colors.black,width: 3,
+                ),
+                ClipRect(
+                  clipBehavior: Clip.hardEdge,
+                  child: Transform(
+                    alignment: Alignment.center,
+                    transform: Matrix4.rotationZ(math.pi),
+                    child: CustomPaint(
+                      painter: FlipBookPainter(_offsets),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width/2,
+                        height:MediaQuery.of(context).size.height/2,
+                        //color: Colors.red[50],
+
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-
-
-        ],
-      )
-
+          const Divider(thickness: 2, color: Colors.black,height: 1,),
+          const SizedBox(height: 10,),
+          _buildButtonUI(100.0,50.0, Colors.green,"Remove"),
+          const SizedBox(height: 10,),
+        ])
 
       // Container(
       //   padding: const EdgeInsets.only(left: 20,right: 20),
@@ -119,19 +179,25 @@ class _HomePageState extends State<HomePage> {
     );
   }
   Widget _buildButtonUI( double btnWidth, double btnHeight, Color btnBackgroundColor,btnName){
-    return   Container(
-       width: 100, height: 45,
-       alignment: Alignment.center,
-       decoration: BoxDecoration(
-         color: btnBackgroundColor,
-         borderRadius: BorderRadius.circular(10)
-       ),
-       child: getText(btnName, 16, FontWeight.w400, Colors.black),
+    return   Padding(
+      padding: const EdgeInsets.only(left:10),
+      child: InkWell(
+        onTap: (){
+          _offsets.clear();
+        },
+        child: Container(
+           width: 100, height: 45,
+           alignment: Alignment.center,
+           decoration: BoxDecoration(
+             color: btnBackgroundColor,
+             borderRadius: BorderRadius.circular(10)
+           ),
+           child: getText(btnName, 16, FontWeight.w400, Colors.black),
+        ),
+      ),
     );
   }
 }
-
-
 
 class FlipBookPainter extends CustomPainter{
   final offsets;
